60d994ab8e28d265241861d60631aba0a1f3e1ad
MaddyTing <37033263+MaddyTing@users.noreply.github.com>
2019-03-11 10:07:19 -0700

    Add files via upload

---
+++ graphics2.py
@@ -0,0 +1,33 @@
+# Draw the gradient rainbow in the solution.
+# To show only the top half of the Circles, use drawing order!
+
+# Sky
+Rect(0, 0, 400, 400, fill='lightSkyBlue')
+
+# Rainbow
+x = 200
+y = 300
+z = 200
+
+Circle(x, y, z, fill=gradient('white', 'red', 'white', start='top'))
+z = z - 30
+
+Circle(x, y, z, fill=gradient('white', 'orange', 'white', start='top'))
+z = z - 30
+
+Circle(x, y, z, fill=gradient('white', 'yellow', 'white', start='top'))
+z = z - 30
+
+Circle(x, y, z, fill=gradient('white', 'green', 'white', start='top'))
+z = z - 30
+
+Circle(x, y, z, fill=gradient('white', 'blue', 'white', start='top'))
+z = z - 30
+
+Circle(x, y, z, fill=gradient('white', 'indigo', 'white', start='top'))
+z = z - 30
+
+Circle(x, y, z, fill=gradient('white', 'violet', 'white', start='top'))
+
+#Grass
+Rect(0, 300, 400, 100, fill=gradient('limeGreen', 'forestGreen', start='top') )
