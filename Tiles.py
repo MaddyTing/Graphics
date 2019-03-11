8c485f868f74ea2afa6e68d24af179e332e17b85
MaddyTing <37033263+MaddyTing@users.noreply.github.com>
2019-03-11 10:10:54 -0700

    Update Tiles.py

--- Tiles.py
+++ Tiles.py
@@ -2,13 +2,12 @@ x = 25
 y = 150
 z = 275
 
-### Modify Your Code Here ###
+# Top Row
 Rect(x, x, 100, 100, fill=gradient('red', 'yellow', start='left-top'))
 Rect(y, x, 100, 100, fill=gradient('red', 'yellow', start='top'))
 Rect(z, x, 100, 100, fill=gradient('red', 'yellow', start='right-top'))
 
-# Here is the first of the three rectangles in the middle row.
-### Modify & Place Your Code Here ###
+# Middle Row
 Rect(x, y, 100, 100, fill=gradient('red', 'yellow', start='left'))
 Rect(y, y, 100, 100, fill=gradient('red', 'yellow'))
 Rect(z, y, 100, 100, fill=gradient('red', 'yellow', start='right'))
