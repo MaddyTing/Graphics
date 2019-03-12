05b9e211c6d8d1d6dca2288ba1b266cd4abb089a
MaddyTing <37033263+MaddyTing@users.noreply.github.com>
2019-03-12 10:17:14 -0700

    Add files via upload

---
+++ graphics4.py
@@ -0,0 +1,7 @@
+x = 200
+
+Circle(x, x, 150, fill=gradient('red', 'darkRed', start='left'))
+Circle(x, x, 120, fill=gradient('white', 'darkGrey', start='left'))
+Circle(x, x, 90, fill=gradient('red', 'darkRed', start='left'))
+Circle(x, x, 60, fill=gradient('blue', 'darkBlue', start='left'))
+Star(x, x, 60, 5, fill=gradient('white', 'darkGrey', start='left'))
