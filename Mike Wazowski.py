cec3a8a0b677e261cf2a648d9f2f334852549260
MaddyTing <37033263+MaddyTing@users.noreply.github.com>
2019-03-14 10:21:11 -0700

    Update Mike Wazowski.py

--- Mike Wazowski.py
+++ Mike Wazowski.py
@@ -1,33 +1,33 @@
 # Pixar's Mike Wazowski from Monster's Inc
 
+# Background
 Rect(0, 0, 400, 400, fill='crimson')
 Line(0, 200, 400, 200, fill='darkRed', opacity=20, lineWidth=400, dashes=(5, 75))
 
-#Horns
+# Horns
 Polygon(135, 90, 130, 125, 150, 105, fill=gradient('whiteSmoke', 'khaki', start='top'))
 Polygon(265, 90, 270, 125, 250, 105, fill=gradient('whiteSmoke', 'khaki', start='top'))
 
-#Arms
+# Arms
 Line(112, 240, 105, 265, lineWidth=8, fill=gradient('mediumSeaGreen', 'greenYellow', start='left'))
 Line(293, 225, 290, 310, lineWidth=8, fill=gradient('mediumSeaGreen', 'greenYellow', start='left'))
 Line(106, 260, 110, 310, lineWidth=8, fill=gradient('mediumSeaGreen', 'greenYellow', start='left'))
 Line(110, 300, 120, 310, fill=gradient('mediumSeaGreen', 'greenYellow', start='left'), lineWidth=8)
 Oval(110, 310, 15, 25, fill=gradient('mediumSeaGreen', 'greenYellow', start='left'))
-
 Line(290, 300, 280, 310, fill=gradient('mediumSeaGreen', 'greenYellow', start='left'), lineWidth=8)
 Oval(290, 310, 15, 25, fill=gradient('mediumSeaGreen', 'greenYellow', start='left'))
 
-#Legs
+# Legs
 Line(170, 300, 170, 360, fill=gradient('mediumSeaGreen', 'greenYellow', start='left'), lineWidth=15)
 Line(230, 300, 230, 360, fill=gradient('mediumSeaGreen', 'greenYellow', start='left'), lineWidth=15)
 Oval(200, 333, 55, 110, fill='crimson')
 Oval(170, 360, 30, 20, fill=gradient('mediumSeaGreen', 'greenYellow', start='left'))
 Oval(230, 360, 30, 20, fill=gradient('mediumSeaGreen', 'greenYellow', start='left'))
 
-#mainbody
+# Main Body
 Oval(200, 200, 200, 220, fill=gradient('greenYellow', 'greenYellow', 'mediumSeaGreen'))
 
-#smile
+# Smile
 Circle(155, 233, 10, fill='darkGreen')
 Circle(155, 235, 10, fill='greenYellow')
 Circle(245, 233, 10, fill='darkGreen')
@@ -36,7 +36,7 @@ Circle(245, 235, 10, fill='greenYellow')
 Oval(200, 232, 100, 50, fill='darkGreen')
 Oval(200, 230, 100, 50, fill='greenYellow')
 
-#Eye
+# Eye
 Circle(200, 160, 50, fill='white')
 Circle(200, 160, 25, fill=gradient('mediumSeaGreen', 'seaGreen'))
 Star(200, 160, 25, 25, roundness=50, fill=gradient('springGreen', 'mediumSeaGreen'))
